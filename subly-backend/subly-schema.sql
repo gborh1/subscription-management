bfe62d3e80614d7a4044d3fd61b89e74d8bfd57c
gborh1 <gborh1@gmail.com>
2021-04-15 18:19:59 -0700

    added schema

---
+++ subly-backend/subly-schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE users (
+  username VARCHAR(25) PRIMARY KEY,
+  password TEXT NOT NULL,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL
+    CHECK (position('@' IN email) > 1),
+  has_paid BOOLEAN NOT NULL DEFAULT FALSE,
+  image_url TEXT
+);
+
+CREATE TABLE admins (
+  username VARCHAR(25) PRIMARY KEY,
+  password TEXT NOT NULL,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL
+    CHECK (position('@' IN email) > 1),
+  image_url TEXT
+);
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  price INTEGER CHECK (salary >= 0),
+  description TEXT,
+  image_url TEXT,
+);
+
+CREATE TABLE subscriptions (
+  username VARCHAR(25)
+    REFERENCES users ON DELETE CASCADE,
+  job_id INTEGER
+    REFERENCES jobs ON DELETE CASCADE,
+  PRIMARY KEY (username, job_id)
+);
